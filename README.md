17abc1cad33260266414d9544805f4f8bd445f0f
MrTimedying <logarzoantonio@gmail.com>
2025-05-03 09:43:46 +0200

    Adjusted the sidebar yet again in consistence with the new npform structure. Updated also the readme finally

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Fulcrum Version 0.3.0-alpha
+# Fulcrum Version 0.9.0-alpha
 
 [![License: CC BY-NC-ND 4.0](https://img.shields.io/badge/License-CC%20BY--NC--ND%204.0-lightgrey.svg)](https://creativecommons.org/licenses/by-nc-nd/4.0/)
 
@@ -15,16 +15,11 @@ Fulcrum is a management software for experts in the field of movement sciences, 
 
 Creating a management solution that will allow to switfly create, compare and analyze data on single persons, multiple or groups or teams and produce document outputs that can be shared among different professional that contribute to the overall wellbeing and success in the field of movement.
 
-### What's the future
+### Roadmap
 
-At the moment the app is in heavy development. Implementing the basic functionalities is the first big milestone. The app has been rewritten from scratch compared to the previous version. 
-Here is a brief Roadmap of the future features and big updates:
+The app is closer to having its basic functionalities ready. There are still some features that need to be implemented, such as the data analysis or the ability to print a full profile or an intervention into a exportable PDF formatted in LaTex. Next in line:
 
-1.	First goal of having the basic functionalitis: creating a profile, creating multiple interventions for each person, handling one person at the time, saving the workspace locally to resume at runtime;
-2. Implementing various quality of life functionalities that will further distance the app from non-specific competitors such as spreadsheets or other not dedicated solutions:
-	* Adding the possibility to quickly duplicate sessions, microcycles or phases of work;
-	* Adding the possibility to order the entire flow diagram based on specific criteria;
-	* Adding the option to templating either phases, microcycles or sessions to quickly re-use them for other interventions [!important];
-3. Further down the road the overall goal is to implement data analysis capabilities for evidence based, data oriented decision making in terms of designing interventions and reading outcomes (which is crucial for every professional figure);
-
+1. Implementing a sound local storage management that doesn't uniquely relies on Zustand;
+2. Implementing a data analysis module that will allow to compare and analyze data within an intervention per patient;
+3. Implementing a full profile export to PDF;
 
--- client/src/components/sidebar.jsx
+++ client/src/components/sidebar.jsx
@@ -18,79 +18,88 @@ function Sidebar() {
     weight: "",
     status: "",
   });
-  
+
   const clientList = useFlowStore((state) => state.patients);
-  const {patientId, setPatientId, setTrailingPatientId} = useFlowStore();
+  const { patientId, setPatientId, setTrailingPatientId } = useFlowStore();
 
   const handleListParsing = (event) => {
+    
     const inputValue = event.target.value;
     setQuery(inputValue);
-  
-    const searchWords = inputValue.toLowerCase().split(' ');
-  
-    const containsWord = (word, value) => {
-      const name = value?.name?.toLowerCase() || '';
-      const surname = value?.surname?.toLowerCase() || '';
-      return (name + surname).includes(word);
-    };    
-  
-    const filteredList = Object.entries(clientList)
-      .filter(([_, value]) => 
-        searchWords.every(word => containsWord(word, value)) 
-      );
+    const searchWords = inputValue.trim().toLowerCase().split(/\s+/);
 
-    console.log(clientList); // Inspect the entire client list
-    console.log(filteredList);
-  
+    const containsWord = (word, value) => {
+      const fullText =
+        `${value.name} ${value.surname} ${value.status}`.toLowerCase();
+      return fullText.includes(word);
+    };
+
+    const filteredList = Object.entries(clientList).filter(([_, value]) =>
+      searchWords.every((word) => containsWord(word, value))
+    );
+
     setFilteredClientList(filteredList);
   };
-  
-  const handleSelection = (chosenPatient) => {
 
+  const handleSelection = (chosenPatient) => {
     if (patientId === chosenPatient) {
       return setPatientId("");
     } else {
-    return setPatientId(chosenPatient);
+      return setPatientId(chosenPatient);
     }
   };
-  
 
   return (
-    <div className="w-1/5 bg-neutral-800 py-4 h-full min-w-80" style={{borderRight: "solid 1px rgb(53 51 51)"}}>
-      <div className="flex flex-row flex-wrap items-center pb-2" >
+    <div
+      className="w-1/5 bg-neutral-800 py-4 h-full min-w-80"
+      style={{ borderRight: "solid 1px rgb(53 51 51)" }}
+    >
+      <div className="flex flex-row flex-wrap items-center pb-2">
         <input
           type="text"
           placeholder="Search..."
           value={query}
           onChange={handleListParsing}
           className="rounded-full bg-zinc-900 text-gray-200"
-          style={{minWidth: "150px", paddingLeft: "15px", margin: "5px"}}
+          style={{ minWidth: "150px", paddingLeft: "15px", margin: "5px" }}
         />
-        <MyDropdown formData={formData} setFormData={setFormData} setFetchingSwitch={setFetchingSwitch} />
+        <MyDropdown
+          formData={formData}
+          setFormData={setFormData}
+          setFetchingSwitch={setFetchingSwitch}
+        />
       </div>
       <div className="flex flex-col h-5/6">
-        <h2 className="text-slate-300 bg-zinc-900 pl-2 text-m font-mono text-xs py-2" style={{borderBottom: "solid 1px #1c1c1c"}}>Patients List</h2>
-        <ul className="bg-zinc-900 p-5 rounded-lg mt-2 mx-2 h-full" style={{borderBottom: "solid 2px rgb(53 51 51)"}}>
-          
-          {filteredClientList.length === 0 ? 
-          Object.entries(clientList).map(([key,value]) => (
-            <li key={key} className={` ${patientId === key ? 'text-slate-600 text-s' : 'text-stone-200 text-xs '} `} onClick={() => handleSelection(key)}>
-              {/* Create a Link for each patient */}
-              <Link to={patientId === key ? '/' : `/patients/${key}`}>
-                {value.name} {value.surname}
-              </Link>
-            </li>
-          )) : 
-          query !== '' ? (<li>No results found for "{query}"</li>) 
-          :
-          filteredClientList.map(([key,value]) => (
-            <li key={key} className={` ${patientId === key ? 'text-slate-600 text-s' : 'text-stone-200 text-xs '} `} onClick={() => handleSelection(key)}>
-              {/* Create a Link for each patient */}
-              <Link to={patientId === key ? '/' : `/patients/${key}`}>
-                {value.name} {value.surname}
-              </Link>
-            </li>
-          ))  }
+        <h2
+          className="text-slate-300 bg-zinc-900 pl-2 text-m font-mono text-xs py-2"
+          style={{ borderBottom: "solid 1px #1c1c1c" }}
+        >
+          Patients List
+        </h2>
+        <ul
+          className="bg-zinc-900 p-5 rounded-lg mt-2 mx-2 h-full"
+          style={{ borderBottom: "solid 2px rgb(53 51 51)" }}
+        >
+          {(query ? filteredClientList : Object.entries(clientList)).map(
+            ([key, value]) => (
+              <li
+                key={key}
+                className={`${
+                  patientId === key
+                    ? "text-slate-600 text-s"
+                    : "text-stone-200 text-xs"
+                }`}
+                onClick={() => handleSelection(key)}
+              >
+                <Link to={patientId === key ? "/" : `/patients/${key}`}>
+                  {value.name} {value.surname}
+                </Link>
+              </li>
+            )
+          )}
+          {query && filteredClientList.length === 0 && (
+            <li className="text-stone-200">No results found for "{query}"</li>
+          )}
         </ul>
       </div>
     </div>
